ec41cf1739cc639e39b9b07143d5c54df232f4a4
Paul <paulereverend@gmail.com>
2021-02-28 19:12:09 +0100

    [PLE] implémentation des tabs "tips liké" et "mes tips"

--- src/app/components/conseil/conseil.component.ts
+++ src/app/components/conseil/conseil.component.ts
@@ -11,6 +11,7 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class ConseilComponent implements OnInit {
 
+  allTips: Tips[];
   tipsList: Tips[];
 
   constructor(private titreService: TitreService, private tipsService: TipsService, public userService: UserService) { }
@@ -34,5 +35,36 @@ export class ConseilComponent implements OnInit {
   }
   isLiked(tip) {
     return this.userService.loggedUser?.likedTips.filter(t => t.id === tip.id).length > 0;
+  }
+  switchTab(tab: string) {
+    this.tipsList = null;
+    let all = document.getElementById('all');
+    let liked = document.getElementById('liked');
+    let mine = document.getElementById('mine');
+    all.className = 'button rose'
+    liked.className = 'button rose'
+    mine.className = 'button rose'
+    document.getElementById(tab).className = "button bg-rose";
+
+    switch (tab) {
+      case 'all':
+        if (!this.allTips) {
+          this.tipsService.getAll().subscribe(tips => {
+            this.tipsList = tips;
+            this.allTips = tips;
+          });
+        } else {
+          this.tipsList = this.allTips;
+        }
+        break;
+      case 'liked':
+        this.tipsList = this.userService.loggedUser.likedTips;
+        break;
+      case 'mine':
+        this.tipsList = this.userService.loggedUser.tips;
+        break;
+      default:
+        break;
+    }
   }
 }
