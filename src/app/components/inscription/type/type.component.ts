4fa6a4f2ce4aeafa77bbff5b514945fc0c589ee6
Paul <paulereverend@gmail.com>
2021-03-04 14:33:42 +0100

    [PLE] envoi du formulaire + verification doublon

--- src/app/components/inscription/type/type.component.ts
+++ src/app/components/inscription/type/type.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-type',
@@ -10,11 +11,17 @@ import { ActivatedRoute } from '@angular/router';
 export class TypeComponent implements OnInit {
 
   ortho: boolean = false;
+  isLoading = false;
   registerForm = new FormGroup({
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    city: new FormControl(),
 
   });
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private userService: UserService, private router: Router) {
     this.route.paramMap.subscribe(params => {
       if (params.get('type') === 'ortho') {
         this.ortho = true;
@@ -25,7 +32,16 @@ export class TypeComponent implements OnInit {
   ngOnInit() {
   }
   register() {
-
+    this.isLoading = true;
+    this.userService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigate(['/accueil']);
+    },
+      error => {
+        this.isLoading = false;
+        this.registerForm.get('email').setErrors({
+          used: true,
+        })
+      });
   }
 
 }
