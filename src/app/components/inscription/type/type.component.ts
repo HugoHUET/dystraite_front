d6e1b747218dfa2cce0c8fd67538121a32a6b363
Paul <paulereverend@gmail.com>
2021-03-04 14:00:12 +0100

    [PLE] correction du css

--- src/app/components/inscription/type/type.component.ts
+++ src/app/components/inscription/type/type.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-type',
   templateUrl: './type.component.html',
-  styleUrls: ['./../inscription.component.css', './type.component.css']
+  styleUrls: ['./type.component.css']
 })
 export class TypeComponent implements OnInit {
 
   ortho: boolean = false;
+  registerForm = new FormGroup({
 
-  constructor(private route: ActivatedRoute) { 
+  });
+
+  constructor(private route: ActivatedRoute) {
     this.route.paramMap.subscribe(params => {
-			if (params.get('type') === 'ortho') {
-				this.ortho = true;
-			}
+      if (params.get('type') === 'ortho') {
+        this.ortho = true;
+      }
     });
   }
 
   ngOnInit() {
+  }
+  register() {
+
   }
 
 }
