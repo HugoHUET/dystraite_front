72d4430ce097e2e9fad638944878c2fe7cc334ee
Matteo Lecuit <matteo@lecuit.fr>
2021-03-24 16:23:57 +0100

    remove token if token is invalid

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -64,8 +64,12 @@ export class UserService {
   }
   loadLoggedUser() {
     if (this.isTokenAvailable()) {
-      this.httpService.get(this.REST_API_SERVER + "loggedUser").subscribe((user: User) => {
+      this.httpService.get(this.REST_API_SERVER + 'loggedUser').subscribe((user: User) => {
         this.loggedUser = user;
+      }, (error: any) => {
+        if (error) {
+          localStorage.removeItem(tokenKey);
+        }
       });
     }
   }
@@ -84,52 +88,52 @@ export class UserService {
       return this.httpService.get(this.REST_API_SERVER + "?lastname=" + lastname).pipe(
           map((res: User) => res));
   }
-  
+
   getUserByFirstname(firstname: string): Observable<User> {
       return this.httpService.get(this.REST_API_SERVER + "?firstname=" + firstname).pipe(
           map((res: User) => res));
   }
-  
+
   getUserByBirthdate(birthdate: string): Observable<User> {
       return this.httpService.get(this.REST_API_SERVER + "?birthdate=" + birthdate).pipe(
           map((res: User) => res));
   }
-  
+
   getUserByLatitude(latitude: number): Observable<User> {
       return this.httpService.get(this.REST_API_SERVER + "?latitude=" + latitude).pipe(
           map((res: User) => res));
   }
-  
+
   getUserByLongitude(longitude: number): Observable<User> {
       return this.httpService.get(this.REST_API_SERVER + "?longitude=" + longitude).pipe(
           map((res: User) => res));
   }
-  
+
   getUserByCity(city: string): Observable<User> {
       return this.httpService.get(this.REST_API_SERVER + "?city=" + city).pipe(
           map((res: User) => res));
   }
-  
+
   getUserByZipcode(zipcode: number): Observable<User> {
       return this.httpService.get(this.REST_API_SERVER + "?zipcode=" + zipcode).pipe(
           map((res: User) => res));
   }
-  
+
   getUserByPassword(password: string): Observable<User> {
       return this.httpService.get(this.REST_API_SERVER + "?password=" + password).pipe(
           map((res: User) => res));
   }
-  
+
   getUserByRole(role: string): Observable<User> {
       return this.httpService.get(this.REST_API_SERVER + "?role=" + role).pipe(
           map((res: User) => res));
   }
-  
+
   getUserByPhoto(photo: any): Observable<User> {
       return this.httpService.get(this.REST_API_SERVER + "?photo=" + photo).pipe(
           map((res: User) => res));
   }
-  
+
   getUserBySpeechtherapist(speechtherapist: any): Observable<User> {
       return this.httpService.get(this.REST_API_SERVER + "?speechtherapist=" + speechtherapist).pipe(
           map((res: User) => res));
